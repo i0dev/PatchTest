4e0fa9e95b82a60badd22661ed702473f78280c0
i0Exception <AndrewOMagnuson@gmail.com>
2022-06-03 10:17:22 -0500

    Cleaned up and changed default monster config

--- src/main/java/com/i0dev/plugin/patchtest/config/MonsterConfig.java
+++ src/main/java/com/i0dev/plugin/patchtest/config/MonsterConfig.java
@@ -1,16 +1,13 @@
 package com.i0dev.plugin.patchtest.config;
 
 import com.i0dev.plugin.patchtest.object.config.ConfigEnchantedItemStack;
-import com.i0dev.plugin.patchtest.object.config.ConfigItemStack;
-import com.i0dev.plugin.patchtest.object.config.PotionEffect;
 import com.i0dev.plugin.patchtest.object.config.MonsterSpawnTime;
+import com.i0dev.plugin.patchtest.object.config.PotionEffect;
 import com.i0dev.plugin.patchtest.template.AbstractConfiguration;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class MonsterConfig extends AbstractConfiguration {
     public MonsterConfig(String path) {
@@ -20,12 +17,44 @@ public class MonsterConfig extends AbstractConfiguration {
 
     @Override
     protected void setValues() {
-        config.set("monsters", Arrays.asList(new MonsterSpawnTime(Collections.singletonMap(15, 1), "&cGuardian", EntityType.SKELETON, 20,
-                Arrays.asList(new PotionEffect("WATER_BREATHING", 3, 10000), new PotionEffect("SPEED", 3, 10000)),
+        Map<Integer, Integer> spawnTimes = new LinkedHashMap<>();
+        spawnTimes.put(10, 2);
+        spawnTimes.put(30, 2);
+        spawnTimes.put(45, 2);
+        spawnTimes.put(60, 2);
+        spawnTimes.put(120, 4);
+        spawnTimes.put(300, 5);
+        spawnTimes.put(400, 5);
+        spawnTimes.put(500, 5);
+        spawnTimes.put(600, 5);
+        spawnTimes.put(700, 5);
+        spawnTimes.put(800, 5);
+        spawnTimes.put(900, 5);
+        spawnTimes.put(1000, 10);
+        spawnTimes.put(1200, 10);
+        spawnTimes.put(1400, 10);
+        spawnTimes.put(1600, 10);
+        spawnTimes.put(1800, 10);
+        spawnTimes.put(2000, 20);
+
+
+        config.set("monsters", Arrays.asList(new MonsterSpawnTime(
+                spawnTimes,
+                "&cGuardian",
+                EntityType.ZOMBIE,
+                100,
+                Arrays.asList(
+                        new PotionEffect("WATER_BREATHING", 3, 10000),
+                        new PotionEffect("SPEED", 3, 10000)
+                ),
                 new ConfigEnchantedItemStack(
                         Material.DIAMOND_HELMET,
                         "Helmet",
-                        new ArrayList<>(),
+                        Arrays.asList(
+                                "&cAquifier",
+                                "&cFatty",
+                                "&cLumination"
+                        ),
                         0,
                         true,
                         Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
@@ -33,7 +62,11 @@ public class MonsterConfig extends AbstractConfiguration {
                 new ConfigEnchantedItemStack(
                         Material.DIAMOND_CHESTPLATE,
                         "Chestplate",
-                        new ArrayList<>(),
+                        Arrays.asList(
+                                "&cVigorous",
+                                "&cHealth Boost",
+                                "&cHaste"
+                        ),
                         0,
                         true,
                         Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
@@ -41,7 +74,11 @@ public class MonsterConfig extends AbstractConfiguration {
                 new ConfigEnchantedItemStack(
                         Material.DIAMOND_LEGGINGS,
                         "Leggings",
-                        new ArrayList<>(),
+                        Arrays.asList(
+                                "&cReplenish",
+                                "&cNether Skin",
+                                "&cResistance"
+                        ),
                         0,
                         true,
                         Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
@@ -49,7 +86,10 @@ public class MonsterConfig extends AbstractConfiguration {
                 new ConfigEnchantedItemStack(
                         Material.DIAMOND_BOOTS,
                         "Boots",
-                        new ArrayList<>(),
+                        Arrays.asList(
+                                "&cAgility",
+                                "&cJelly"
+                        ),
                         0,
                         true,
                         Collections.singletonMap("DEPTH_STRIDER", 3)
@@ -60,49 +100,7 @@ public class MonsterConfig extends AbstractConfiguration {
                         new ArrayList<>(),
                         0,
                         true,
-                        Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
-                )
-        ).serialize(), new MonsterSpawnTime(Collections.singletonMap(20, 1), "&cGuardian", EntityType.ZOMBIE, 20,
-                Arrays.asList(new PotionEffect("WATER_BREATHING", 3, 10000), new PotionEffect("SPEED", 3, 10000)),
-                new ConfigEnchantedItemStack(
-                        Material.DIAMOND_HELMET,
-                        "Helmet",
-                        new ArrayList<>(),
-                        0,
-                        true,
-                        Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
-                ),
-                new ConfigEnchantedItemStack(
-                        Material.DIAMOND_CHESTPLATE,
-                        "Chestplate",
-                        new ArrayList<>(),
-                        0,
-                        true,
-                        Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
-                ),
-                new ConfigEnchantedItemStack(
-                        Material.DIAMOND_LEGGINGS,
-                        "Leggings",
-                        new ArrayList<>(),
-                        0,
-                        true,
-                        Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
-                ),
-                new ConfigEnchantedItemStack(
-                        Material.DIAMOND_BOOTS,
-                        "Boots",
-                        new ArrayList<>(),
-                        0,
-                        true,
-                        Collections.singletonMap("DEPTH_STRIDER", 3)
-                ),
-                new ConfigEnchantedItemStack(
-                        Material.DIAMOND_SWORD,
-                        "Sword",
-                        new ArrayList<>(),
-                        0,
-                        true,
-                        Collections.singletonMap("PROTECTION_ENVIRONMENTAL", 5)
+                        Collections.singletonMap("DAMAGE_ALL", 15)
                 )
         ).serialize()));
     }
